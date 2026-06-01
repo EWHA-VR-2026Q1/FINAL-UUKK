e5fa5939232ddde6ca1163d280490f14876552fc
uri-git23 <uryuuri21@gmail.com>
2026-06-01 20:48:48 +0900

    Fix: 머지 컨플릭트 해결

--- Final_project/Assets/Final/Scenes_yr/Scripts/SpiderNamingManager.cs
+++ Final_project/Assets/Final/Scenes_yr/Scripts/SpiderNamingManager.cs
@@ -29,10 +29,6 @@ public class SpiderNamingManager : MonoBehaviour
     private Color selectedColor = new Color(0.29f, 0.56f, 1.00f);
     private bool  nameConfirmed = false;
 
-    private bool nameConfirmed = false;
-
-    private bool nameConfirmed = false;
-
     void Start()
     {
         btnPip.onClick.AddListener(()   => OnSelectName("Pip"));
